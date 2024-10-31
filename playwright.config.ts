a7305feb9786d90aeb70c52eb7bc1d345c0ac138
osawa-koki <koki@osawa.dev>
2024-10-31 10:50:25 +0900

    small fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -65,7 +65,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: isCi ? 'npm run server' : 'npm run dev',
+    command: isCi ? 'npm run start' : 'npm run dev',
     url: 'http://127.0.0.1:3000',
     reuseExistingServer: !isCi,
   },
